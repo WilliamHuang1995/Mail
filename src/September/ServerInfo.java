48c5a1304b714c3f68b7c01d4b0d00d7fdea2b8b
user <user@192.168.13.102>
2016-09-06 14:42:25 +0800

    Changed Server Info to correct one

--- src/September/ServerInfo.java
+++ src/September/ServerInfo.java
@@ -1,12 +1,12 @@
 // =======================================================================================================================================
-// Copyright © 2012, Oracle and/or its affiliates. All rights reserved. 
+// Copyright ï¿½ 2012, Oracle and/or its affiliates. All rights reserved. 
 // 
 // This software and related documentation are provided under a license agreement containing restrictions on use and disclosure and are protected by intellectual
 // property laws. Except as expressly permitted in your license agreement or allowed by law, you may not use, copy, reproduce, translate, broadcast, modify,
 // license, transmit, distribute, exhibit, perform, publish, or display any part, in any form, or by any means. Reverse engineering, disassembly, or decompilation of
 // this software, unless required by law for interoperability, is prohibited.
 // 
-// The Sample Code is for illustrative purposes only. This Sample code is provided ‘AS-IS’ and provided without Warranty of any kind. Sample code are not
+// The Sample Code is for illustrative purposes only. This Sample code is provided ï¿½AS-ISï¿½ and provided without Warranty of any kind. Sample code are not
 // covered by STANDARD SUPPORT SERVICES.
 // 
 // Oracle is a registered trademark of Oracle Corporation and/or its affiliates. Other names may be trademarks of their respective owners.
@@ -20,7 +20,7 @@ package September;
  */
 public class ServerInfo {
       // Update the following fields with information for your server
-	final String connectString = "http://hodakadb:7001/Agile/";
+	final String connectString = "http://192.168.13.250:7001/Agile/";
 	final String username = "admin";
-	final String password = "agile934";
+	final String password = "agile933";
 }
