afcc93de74d425c087ec3f46c66c7e73759d9b3a
user <user@192.168.13.102>
2016-09-06 14:43:47 +0800

    Can now generate a hyperlink to the correct change order

--- src/September/EmailNotify.java
+++ src/September/EmailNotify.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Multipart;
@@ -27,6 +29,8 @@ import com.agile.api.APIException;
 import com.agile.api.AgileSessionFactory;
 import com.agile.api.IAdmin;
 import com.agile.api.IAgileSession;
+import com.agile.api.IUser;
+import com.agile.api.UserConstants;
 import com.anselm.plm.util.AUtil;
 import com.anselm.plm.utilobj.Ini;
 import com.anselm.plm.utilobj.LogIt;
@@ -34,11 +38,12 @@ import com.anselm.plm.utilobj.LogIt;
 import sun.rmi.runtime.Log;
 
 public class EmailNotify extends ServerInfo {
-	IAgileSession m_session;
+	static IAgileSession m_session;
 	IAdmin m_admin;
 	AgileSessionFactory m_factory;
 	Properties props;
 	static HashMap<String, ArrayList<String>> usersMap = new HashMap<String, ArrayList<String>>();
+	static final String URL = "<a href='http://192.168.13.250:7001/Agile/PLMServlet?action=OpenEmailObject&isFromNotf=true&module=ChangeHandler&classid=6000&objid=";
 
 	public EmailNotify() {
 	}
@@ -85,11 +90,21 @@ public class EmailNotify extends ServerInfo {
 
 	private static void sendMail(Properties props) {
 		try {
-		
+
 			Iterator iter = usersMap.entrySet().iterator();
 			while (iter.hasNext()) {
 				Map.Entry pair = (Map.Entry) iter.next();
-				System.out.println(pair.getKey() + " = " + pair.getValue());
+				// System.out.println(pair.getKey() + " = " + pair.getValue());
+				IUser user;
+				String key = (String) pair.getKey();
+				try {
+					user = (IUser) m_session.getObject(UserConstants.CLASS_USER_BASE_CLASS, key);
+					String userEmail = (String) user.getValue(UserConstants.ATT_GENERAL_INFO_EMAIL);
+					System.out.println(userEmail);
+				} catch (APIException e) {
+					System.out.println("user not found");
+					e.printStackTrace();
+				}
 
 				Session mailSession = Session.getDefaultInstance(props, null);
 				Transport transport = mailSession.getTransport();
@@ -101,20 +116,31 @@ public class EmailNotify extends ServerInfo {
 				MimeBodyPart textPart = new MimeBodyPart();
 				StringBuffer html = new StringBuffer();
 
-				html.append("\n<a href='http://192.168.13.250:7001/Agile/'>Agile PLM</a>");
-
+				//html.append("\n<a href='http://192.168.13.250:7001/Agile/'>Agile PLM</a>");
+				html.append("<h3>您的PLM待簽核表單總覽</h3>");
+				html.append("<img src='cid:image'/><br>");
 				html.append("<table><tr><td>表單編號</td><td>表單描述</td><td>站別</td><td>已持續時間(天)</td></tr>");
 				ArrayList<String> val = (ArrayList<String>) pair.getValue();
-				while(!val.isEmpty()){
+				while (!val.isEmpty()) {
 					html.append(val.get(0));
 					val.remove(0);
 				}
 				html.append("</table>");
 				textPart.setContent(html.toString(), "text/html; charset=UTF-8");
+				
+				//Oracle Logo
+				MimeBodyPart picturePart = new MimeBodyPart();
+				FileDataSource fds = new FileDataSource("Oracle-logo.png");
+				picturePart.setDataHandler(new DataHandler(fds));
+				picturePart.setFileName(fds.getName());
+				picturePart.setHeader("Content-ID", "<image>");
+				
 				Multipart email = new MimeMultipart();
 				email.addBodyPart(textPart);
+				email.addBodyPart(picturePart);
+				
 				message.setContent(email);
-				message.addRecipient(Message.RecipientType.TO, new InternetAddress("jjsu@anselm.com.tw"));
+				message.addRecipient(Message.RecipientType.TO, new InternetAddress("johnnylee@anselm.com.tw"));
 				message.setFrom(new InternetAddress("william@anselm.com.tw")); // 寄件者
 				transport.connect("william@anselm.com.tw", "epacsenur123");
 				transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
@@ -129,7 +155,6 @@ public class EmailNotify extends ServerInfo {
 		} catch (MessagingException e) {
 			e.printStackTrace();
 		}
-		
 
 	}
 
@@ -137,7 +162,6 @@ public class EmailNotify extends ServerInfo {
 	 * 郵件內容需包括一個表格，表頭欄位為：表單編號(可超連結)、表單描述、站別、已持續時間(天)
 	 */
 	public void getTable(IAgileSession session, Map map) throws Exception {
-
 
 		Ini ini = new Ini("C:/Users/user/Desktop/Anselm/Config.ini");
 		LogIt log = new LogIt("");
@@ -165,24 +189,25 @@ public class EmailNotify extends ServerInfo {
 			ResultSet rs = conA.createStatement().executeQuery(sql);
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int numCols = rsmd.getColumnCount();
-			//for (int i = 1; i <= numCols; i++)
-				//log.log(rsmd.getColumnName(i) + "　" + i);
+			// for (int i = 1; i <= numCols; i++)
+			// log.log(rsmd.getColumnName(i) + " " + i);
 			while (rs.next()) {
 				HashMap<String, String> datarow = new HashMap<String, String>();
 				String user = rs.getString(4);
 				String changeNumber = rs.getString(6);
-				String changeDesc =	rs.getString(10);
-				String status =	rs.getString(8);
+				String changeDesc = rs.getString(10);
+				String status = rs.getString(8);
 				String duration = rs.getString(1);
+				String changeID = rs.getString(12);
 				if (usersMap.containsKey(user)) {
 					ArrayList<String> list = usersMap.get(user);
-					list.add("<tr><td>" + changeNumber + "</td><td>" + changeDesc + "</td><td>"
-							+ status + "</td><td>" + duration + "</td></tr>");
+					list.add("<tr><td>" + URL + changeID + "'>" + changeNumber + "</a></td><td>" + changeDesc
+							+ "</td><td>" + status + "</td><td>" + duration + "</td></tr>");
 					usersMap.put(user, list);
 				} else {
 					ArrayList<String> list = new ArrayList<String>();
-					list.add("<tr><td>" + changeNumber + "</td><td>" + changeDesc + "</td><td>"
-							+ status + "</td><td>" + duration + "</td></tr>");
+					list.add("<tr><td>" + URL + changeID + "'>" + changeNumber + "</a></td><td>" + changeDesc
+							+ "</td><td>" + status + "</td><td>" + duration + "</td></tr>");
 					usersMap.put(user, list);
 				}
 			}
@@ -194,7 +219,7 @@ public class EmailNotify extends ServerInfo {
 				log.log("Agile DB Closed.");
 				log.log("Sending Emails.");
 				sendMail(props);
-				
+
 			} catch (Exception e) {
 			}
 		}
--- src/September/SendMailDemo.java
+++ src/September/SendMailDemo.java
@@ -23,8 +23,6 @@ public class SendMailDemo {
 			props.setProperty("mail.transport.protocol", "smtp");
 			props.setProperty("mail.host", "mail.anselm.com.tw");
 			props.setProperty("mail.protocol.port", "25");
-			// props.setProperty("mail.user", "william@anselm.com.tw");
-			// props.setProperty("mail.password", "epacsenur123");
 			props.setProperty("mail.smtp.auth", "true");
 
 			Session mailSession = Session.getDefaultInstance(props, null);
@@ -34,21 +32,23 @@ public class SendMailDemo {
 			MimeMessage message = new MimeMessage(mailSession);
 
 			// 設定主旨
-			message.setSubject("Test 9/2/2016");
-
+			message.setSubject("您的PLM待簽核表單總覽-2016/09/05");
+			
 			// 文字部份，注意 img src 部份要用 cid:接下面附檔的header
 			MimeBodyPart textPart = new MimeBodyPart();
 			StringBuffer html = new StringBuffer();
-
-			html.append("<h2> Dear Jane </h2><br>");
-
-			// html.append("<h3>這是第二行，下面會是圖</h3><br>");
-			html.append("<img src='cid:image'/><br>");
+			html.append("<h3>您的PLM待簽核表單總覽</h3>");
+			html.append("<img src='cid:image'/><br>");	
+			html.append("<table><tr><td>表單編號</td><td>表單描述</td><td>站別</td><td>已持續時間(天)</td></tr>");
+			html.append("<tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>");
+			html.append("<tr><td>1</td><td>2</td><td>3</td><td>4</td></tr>");
+			html.append("<tr><td><a href='http://192.168.13.250:7001/Agile/PCMServlet?fromPCClient=true&module=ChangeHandler&requestUrl=module%3DChangeHandler%26opcode%3DdisplayObject%26classid%3D6000%26objid%3D7153413%26tabid%3D0%26'>test</a></td><td><a href='http://192.168.13.250:7001/Agile/PLMServlet?action=OpenEmailObject&isFromNotf=true&module=ChangeHandler&classid=6000&objid=7153413'>test2</a></td><td>3</td><td>4</td></tr></table>");
+			html.append("<p><a href='http://192.168.13.250:7001/Agile/PCMServlet?fromPCClient=true&module=ChangeHandler&requestUrl=module%3DChangeHandler%26opcode%3DdisplayObject%26classid%3D6000%26objid%3D7153413%26tabid%3D0%26'>點取這裡可直接進入客戶端</a></p>");
 			textPart.setContent(html.toString(), "text/html; charset=UTF-8");
 
 			// 圖檔部份，注意 html 用 cid:image，則header要設<image>
 			MimeBodyPart picturePart = new MimeBodyPart();
-			FileDataSource fds = new FileDataSource("pokemonbgd.png");
+			FileDataSource fds = new FileDataSource("Oracle-logo.png");
 			picturePart.setDataHandler(new DataHandler(fds));
 			picturePart.setFileName(fds.getName());
 			picturePart.setHeader("Content-ID", "<image>");
@@ -58,7 +58,7 @@ public class SendMailDemo {
 			email.addBodyPart(picturePart);
 
 			message.setContent(email);
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress("paul.huang@foamrite.net"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress("wjhuang@ucsd.edu"));
 			message.setFrom(new InternetAddress("william@anselm.com.tw")); // 寄件者
 			transport.connect("william@anselm.com.tw", "epacsenur123");
 			transport.sendMessage(message, message.getRecipients(Message.RecipientType.TO));
