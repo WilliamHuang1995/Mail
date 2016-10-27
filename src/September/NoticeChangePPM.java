5abe3219cea1cc04da2c8a31d580393ce77372cd
William Huang <wjhuang@ucsd.edu>
2016-10-27 10:27:16 +0800

    Push before I make anymore big changes

--- src/September/NoticeChangePPM.java
+++ src/September/NoticeChangePPM.java
@@ -13,7 +13,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -24,7 +23,6 @@ import com.agile.api.IAgileSession;
 import com.agile.api.ICell;
 import com.agile.api.INode;
 import com.agile.api.IProgram;
-import com.agile.api.ITableDesc;
 import com.agile.api.IUser;
 import com.agile.api.UserConstants;
 import com.agile.px.ActionResult;
@@ -35,69 +33,101 @@ import com.agile.px.IEventInfo;
 import com.agile.px.IUpdateEventInfo;
 import com.anselm.plm.util.AUtil;
 import com.anselm.plm.utilobj.Ini;
-import com.anselm.plm.utilobj.LogIt; 
+import com.anselm.plm.utilobj.LogIt;
 
-//this event action is used to detect changes in the title block
+/**
+ * <h1>NoticeChangePPM</h1> The NoticeChangePPM program extends IEventAction and
+ * adds PLM users to an excel file. This excel file will be used as a reference
+ * to send out email notifying users of these new changes This custom PX should
+ * be used to detect the 'Save As' event else it would not work.
+ *
+ * @author William Huang
+ * @version 1.0
+ * @since 2016-09-26
+ */
 public class NoticeChangePPM implements IEventAction {
 	static HSSFSheet spreadsheet;
 	static HSSFWorkbook workbook;
 	static int rowcount = 0;
 	static final String URL = "/PLMServlet?action=OpenEmailObject&isFromNotf=true&classid=18022&objid=";
 
+	/**********************************************************************
+	 * 內部變數 log : 自定義方便debug的log物件 ini : 自定義方便讀取config的程式
+	 **********************************************************************/
+	private LogIt log = null;
+	private Ini ini = null;
+	private String filename;
+	private String fileLocation;
+	private IAgileSession session;
+	ArrayList<String> roleList;
+
+	/*
+	 * Constructor
+	 */
+	public NoticeChangePPM() {
+
+		ini = new Ini();
+		log = new LogIt("Role Change");
+
+		// Initialize filepath
+		Date today = new Date();
+		SimpleDateFormat format = new SimpleDateFormat("MMdd");
+		filename = format.format(today) + ".xlsx";
+		fileLocation = ini.getValue("File Location", "FILEPATH");
+		try {
+			log.setLogFile(ini.getValue("File Location", "LOG_FILE_PATH") + format.format(today) + ".log");
+
+			// initialize workbook as well
+			rowcount = initializeWorkbook(checkExist(), fileLocation, filename);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		// Initialize role list
+		String roleFromConfig = ini.getValue("Settings", "role");
+		roleList = new ArrayList<String>(Arrays.asList(roleFromConfig.split("\\s*,\\s*")));
+
+	}
+
+	private boolean checkExist() {
+		return (new File(fileLocation + filename)).exists();
+	}
+
 	@Override
 	// This code looks for an existing item under the category Parts and throws
 	// and error if it exists.
 	public EventActionResult doAction(IAgileSession session, INode actionNode, IEventInfo event) {
-		LogIt log = new LogIt();
-		Ini ini = new Ini();
-		String str = ini.getValue("Settings", "role");
-		log.log(ini.getValue("Settings", "role"));
-		ArrayList<String> array = new ArrayList<String>(Arrays.asList(str.split("\\s*,\\s*")));
-		log.log(array);
 
+		// The event is an update event
 		IUpdateEventInfo info = (IUpdateEventInfo) event;
-
-		// initialize filename
-		Date today = new Date();
-		SimpleDateFormat format = new SimpleDateFormat("MMdd");
-		String filename = format.format(today) + ".xlsx";
-		String fileLoc = ini.getValue("File Location", "FILEPATH");
-
-		File temp = new File(fileLoc + filename);
-		boolean exists = temp.exists();
-		System.out.println(exists);
-
-		try {
-			rowcount = initializeWorkbook(exists, fileLoc, filename);
-		} catch (Exception e) {
-			log.log("error");
-		}
-		int index = 1;
-		Map<String, Object[]> empinfo = new TreeMap<String, Object[]>();
-		if (!exists) {
-			empinfo.put(index+"", new Object[] { "User Role", "Name", "Email", "Project Name","Project Link" });
-		}
-	
 		// get admin session
 		session = AUtil.getAgileSession(ini, "AgileAP");
+
+		// to be frank, the starting index does not matter.
+		int mappingIndex = 1;
+		Map<String, Object[]> empinfo = new TreeMap<String, Object[]>();
+		if (checkExist()) {
+			empinfo.put(mappingIndex + "",
+					new Object[] { "User Role", "List/MultiList", "Name", "Email", "Project Name", "Project Link" });
+		}
+
 		try {
 			IEventDirtyCell[] cells = info.getCells();
 			IProgram program = (IProgram) session.getObject(info.getDataObject().getAgileClass(),
 					info.getDataObject().getName());
 			for (IEventDirtyCell dirtyCell : cells) {
-				if (array.contains(dirtyCell.getAttribute().getName())) {
+				if (roleList.contains(dirtyCell.getAttribute().getName())) {
 					String attributeName = dirtyCell.getAttribute().getName();
 					ICell cleanCell = program.getCell(dirtyCell.getAttributeId());
-					
-					
-					//gets the objID integer
+
+					// gets the objID integer
 					String objID = program.getId().toString();
 					ArrayList<String> idList = new ArrayList<String>(Arrays.asList(objID.split("\\s*=\\s*")));
-					objID = idList.get(1).substring(0,idList.get(1).indexOf(" "));
-					
-					
-					
-					ArrayList<String> oldValue = new ArrayList(
+					objID = idList.get(1).substring(0, idList.get(1).indexOf(" "));
+
+					ArrayList<String> oldValue = new ArrayList<String>(
 							Arrays.asList(cleanCell.getValue().toString().split("\\s*;\\s*")));
 					ArrayList<String> newValue = new ArrayList<String>(
 							Arrays.asList(dirtyCell.getValue().toString().split("\\s*;\\s*")));
@@ -111,17 +141,18 @@ public class NoticeChangePPM implements IEventAction {
 						IUser user = (IUser) session.getObject(IUser.OBJECT_TYPE, userID);
 						String userEmail = (String) user.getValue(UserConstants.ATT_GENERAL_INFO_EMAIL);
 						String userName = (String) user.getValue(UserConstants.ATT_GENERAL_INFO_FIRST_NAME);
+						String dataType = cleanCell.getDataType() == 4 ? "Single List" : "Multi List";
 						log.log(userName);
 						log.log(userEmail);
 						log.log(attributeName + " " + userName + " " + userEmail + " " + program.getName());
-						empinfo.put(++index + "",
-								new Object[] { attributeName, userName, userEmail, program.getName(),ini.getValue("Server Info", "url")+ URL + objID});
+						empinfo.put(++mappingIndex + "", new Object[] { attributeName, dataType, userName, userEmail,
+								program.getName(), ini.getValue("Server Info", "url") + URL + objID });
 					}
 				}
 			}
 
 			Set<String> keyid = empinfo.keySet();
-			
+
 			for (String key : keyid) {
 				HSSFRow row = spreadsheet.createRow(rowcount++);
 				Object[] objectArr = empinfo.get(key);
@@ -134,11 +165,11 @@ public class NoticeChangePPM implements IEventAction {
 
 			// Write the workbook in file system
 			try {
-				FileOutputStream out = new FileOutputStream(new File(fileLoc + filename));
+				FileOutputStream out = new FileOutputStream(new File(fileLocation + filename));
 				workbook.write(out);
 				out.close();
 				log.log("close");
-				System.out.println("Write Successful");
+				log.log("Write Successful");
 			} catch (IOException e) {
 			}
 
@@ -152,18 +183,19 @@ public class NoticeChangePPM implements IEventAction {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		log.close();
 		return new EventActionResult(event, new ActionResult(ActionResult.NORESULT, null));
 	}
 
 	private int initializeWorkbook(boolean exists, String fileLoc, String filename) throws Exception {
-		int rowcount=0;
+		int rowcount = 0;
 		if (exists) {
 			FileInputStream fis = new FileInputStream(new File(fileLoc + filename));
 			workbook = new HSSFWorkbook(fis);
 			spreadsheet = workbook.getSheetAt(0);
 			// used to append to the current file
-			rowcount = spreadsheet.getLastRowNum()+1;
-			System.out.println("ROWCOUNT: "+rowcount);
+			rowcount = spreadsheet.getLastRowNum() + 1;
+			log.log("ROWCOUNT: " + rowcount);
 			fis.close();
 
 		} else {
